73459f5f5f4a79d9444eed5ff3a544cb5318ba0d
Sergey Smolov <smolov@ispras.ru>
2023-10-12 10:43:33 +0300

    iwls05/iscas: add 'blif' & 'netlist' from orig benchmark
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

---
+++ hdl/iwls05/iscas/netlist/s526.tcl
@@ -0,0 +1,35 @@
+set_attr max_print 1 VLOGPT/VLOGPT-35
+
+set_attribute library ../../library/GSCLib_3.0.lib
+set inv_out INVX2/Y
+
+load ../rtl/s526.v
+elaborate
+
+redirect /dev/null {
+  set clock [define_clock -p 0 -n clock [clock_ports]]
+  external_delay -i 0 -c $clock /designs/*/ports_in/*
+  external_delay -o 0 -c $clock /designs/*/ports_out/*
+  if {[info exists std_load]} {
+    set_attr external_wire_cap [expr 6 * $std_load] /des*/*/ports_out/*
+  }
+
+  set allins [find / -port ports_in/*]
+  set clks [lsearch -glob $allins [clock_ports]]
+  set noclksin [lreplace $allins $clks $clks]
+  set_attr external_driver $inv_out $noclksin
+}
+
+set errorInfo ""
+
+ungroup -flatten -all
+
+synthesize -to_mapped -effort low
+synthesize -incremental -effort high
+
+report timing
+report gates
+
+write -mapped > s526.v
+
+quit
