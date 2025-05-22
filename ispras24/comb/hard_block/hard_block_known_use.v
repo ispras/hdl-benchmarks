ec26f9f2f082dd0e213730a4e5bb30fbb06badde
Григоров Иван Александрович <grigorovia@ispras.ru>
2025-05-22 11:13:05 +0000

    Added trivial designs using hardblocks.

---
+++ ispras24/comb/hard_block/hard_block_known_use.v
@@ -0,0 +1,12 @@
+module hard_block_known_use(A, B, S, Y);
+
+  parameter WIDTH = 4;
+
+  input [WIDTH-1:0] A, B;
+  input S;
+
+  output [WIDTH-1:0] Y;
+  
+  hard_block_known hard_block_known_u(.A(A), .B(B), .S(S), .Y(Y));
+  
+endmodule
