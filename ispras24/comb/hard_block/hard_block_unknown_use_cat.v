f124fc0068418a697e9282f335176fc243c7df44
Григоров Иван Александрович <grigorovia@ispras.ru>
2025-05-30 12:11:24 +0000

    Add tests for undefined modules with non-trivial connections

---
+++ ispras24/comb/hard_block/hard_block_unknown_use_cat.v
@@ -0,0 +1,15 @@
+module hard_block_unknown_use_cat(A1, A2, B, S, Y);
+
+  parameter WIDTH_A = 2;
+  parameter WIDTH_B = 4;
+
+  input [WIDTH_A-1:0] A1;
+  input [WIDTH_A-1:0] A2;
+  input [WIDTH_B-1:0] B;
+  input S;
+
+  output [WIDTH_B-1:0] Y;
+  
+  hard_block_unknown hard_block_unknown_u(.A({A1, A2}), .B(B), .S(S), .Y(Y));
+  
+endmodule
