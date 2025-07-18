08ccf872e5a5e309934dfa46d703f5dabeb98080
Sergey Smolov <smolov@ispras.ru>
2025-07-18 10:05:32 +0300

    Revert "Merge branch 'oc_minirisc_fix' (I. Grigorov)"
    This reverts commit 8d4907e13e46c08dc6962f2f8201f221c32f44fa, reversing
    changes made to ef83242b5f5db7a7e6c3065895d5ad507d6aa3c8.

--- quip/oc_minirisc/ramb4_s4_s4.v
+++ quip/oc_minirisc/ramb4_s4_s4.v
@@ -3,14 +3,14 @@ module RAMB4_S4_S4 (CLKA, CLKB, RSTB, RSTA, DOA, ADDRA, DIA, ENA, WEA, DOB, ADDR
   input CLKB;
   input RSTB;
   input RSTA;
-  output [3:0] DOA;
-  output [9:0] ADDRA;
+  output DOA;
+  output ADDRA;
   input DIA;
   output ENA;
   input WEA;
-  output [3:0] DOB;
-  output [9:0] ADDRB;
-  output [3:0] DIB;
+  output DOB;
+  output ADDRB;
+  output DIB;
   output ENB;
   output WEB;
 endmodule
