47c68c8dcd34b70972d99cf6c75468a4c8e3d5fd
Григоров Иван Александрович <grigorovia@ispras.ru>
2026-01-28 14:16:54 +0000

    ispras24: RTL models for signed div and mod

---
+++ ispras24/comb/div/div_8s_8s_20s.v
@@ -0,0 +1,8 @@
+module div_8s_8s_20s(a, b, c);
+  input signed [7:0] a;
+  input signed [7:0] b;
+  output signed [19:0] c;
+
+  assign c = a / b;
+
+endmodule
---
+++ ispras24/comb/mod/mod_8s_8s_9s.v
@@ -0,0 +1,8 @@
+module mod_8s_8s_9s(a, b, c);
+  input signed [7:0] a;
+  input signed [7:0] b;
+  output signed [8:0] c;
+
+  assign c = a % b;
+
+endmodule
